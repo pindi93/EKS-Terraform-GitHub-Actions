afe000c01562023f7b164135bcf574828a520fa8
pindi93 <86508719+pindi93@users.noreply.github.com>
2025-01-14 12:19:08 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,11 +1,11 @@
 terraform {
-  required_version = "~> 1.9.3"
+  /*required_version = "~> 1.9.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.49.0"
     }
-  }
+  } */
   backend "s3" {
     bucket         = "eksterraform23"
     region         = "us-east-1"
