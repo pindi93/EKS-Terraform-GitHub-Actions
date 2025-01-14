8b6f25b35ff55f679a21359d7cb0eac359c5c654
pindi93 <86508719+pindi93@users.noreply.github.com>
2025-01-14 12:14:48 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,11 +7,11 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "eksterraform23"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
+    #dynamodb_table = "Lock-Files"
+    #encrypt        = true
   }
 }
 
